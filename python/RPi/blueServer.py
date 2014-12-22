b6fade76ec56c4a7e699a617ec43a456e8dbb894
wyle-e-coyote <chrislawrence05@gmail.com>
2014-12-22 03:53:56 +0000

    Updated RPi blue server to use json

--- python/RPi/blueServer.py
+++ python/RPi/blueServer.py
@@ -1,9 +1,10 @@
 from lightblue import *
+import json
 
 s = socket()
 s.bind(("",7))
 s.listen(1)
-advertise("RPi Service", s,RFCOMM)
+advertise("RPi Bluetooth", s,RFCOMM)
 
 print "Listening on %s port %s" % (s.getsockname()[0], s.getsockname()[1])
 
@@ -11,7 +12,10 @@ conn, addr = s.accept()
 print "Connected by", addr
 result = conn.recv(1024)
 conn.send('WTF?!?')
-print "Result: ", result
+
+decoded = json.loads(result)
+
+print "Result: ", decoded['code'] 
 conn.close()
 s.close()
 
